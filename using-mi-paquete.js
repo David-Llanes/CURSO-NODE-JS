a6441e0b1d79148a4bdcdc0aa1d44522e596633e
David-Llanes <david.llanes1019@gmail.com>
2023-09-10 18:16:18 -0700

    Se volvio a agregar using-mi-paquete

---
+++ using-mi-paquete.js
@@ -0,0 +1,8 @@
+const { sumar, restar, multiplicar, saludo1, saludo2 } = require('./mi-paquete')
+
+console.log(sumar(1, 3))
+console.log(restar(7, 3))
+console.log(multiplicar(2, 3))
+
+saludo1('Sasuke')
+saludo2('Naruto', 'Sasuke')
